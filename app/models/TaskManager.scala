4ad2c89f710d904d4b52db3b07e398156badd257
Kirill Grishin <grishin@enrapt.jp>
2014-01-02 02:52:53 +0900

    Introduce a container for a task and its subtasks
    A multiway tree is used as data structure for this container.

--- app/models/TaskManager.scala
+++ app/models/TaskManager.scala
@@ -8,74 +8,62 @@ import play.api.libs.functional.syntax._
 
 object Cypher {
 
-  case class Task(desc: String)
-
-  object Task {
-    implicit val reader = Json.reads[Task]
-}
-
-/*  implicit val */
-
-  case class Response[A](columns: List[String], data: List[List[List[Node[A]]]])
-
-  //implicit val ln = Json.reads[List[Node]]
+  /**
+   * The common structure of the response
+   *
+   * {
+   *   "columns": [ ... ],
+   *   "data": [[...]]
+   * }
+   *
+   * @param columns
+   * @param data
+   * @tparam A This should be a reader for the row;
+   */
+  case class Response[A](columns: List[String], data: List[List[A]])
 
   object Response {
 
-    implicit val reader = (
+    def reader[A](rowReader: Reads[A]) = (
       (__ \ "columns").read[List[String]] ~
-      (__ \ "data").read[List[List[List[Node[Task]]]]](list(list(list(Node.reader(Task.reader)))))
-    )(Response.apply[Task] _)
-
-//    type Data = List[Row]
-
-//    type Row = List[String]
+      (__ \ "data").read[List[List[A]]](list(list(rowReader)))
+    )(Response.apply[A] _)
 
   }
 
+  /**
+   *
+   * @param self
+   * @param data
+   * @tparam A Type parameter for the data type that is stored in a node of the DB
+   */
   case class Node[A](
     self: String,
     data: A
   )
 
   object Node {
-    implicit def reader[A](dataReader: Reads[A]) = (
+
+    /**
+     *
+     * @param dataReader A reader for the class which is stored in the node
+     * @tparam A
+     * @return
+     */
+    def reader[A](dataReader: Reads[A]) = (
       (__ \ "self").read[String] ~
       (__ \ "data").read[A](dataReader)
     )(Node.apply[A] _)
+
   }
-
-
-
-/*  "extensions": {},
-  "paged_traverse": "http://localhost:7474/db/data/node/6/paged/traverse/{returnType}{?pageSize,leaseTime}",
-  "labels": "http://localhost:7474/db/data/node/6/labels",
-  "outgoing_relationships": "http://localhost:7474/db/data/node/6/relationships/out",
-  "traverse": "http://localhost:7474/db/data/node/6/traverse/{returnType}",
-  "all_typed_relationships": "http://localhost:7474/db/data/node/6/relationships/all/{-list|&|types}",
-  "property": "http://localhost:7474/db/data/node/6/properties/{key}",
-  "all_relationships": "http://localhost:7474/db/data/node/6/relationships/all",
-  "self": "http://localhost:7474/db/data/node/6",
-  "properties": "http://localhost:7474/db/data/node/6/properties",
-  "outgoing_typed_relationships": "http://localhost:7474/db/data/node/6/relationships/out/{-list|&|types}",
-  "incoming_relationships": "http://localhost:7474/db/data/node/6/relationships/in",
-  "incoming_typed_relationships": "http://localhost:7474/db/data/node/6/relationships/in/{-list|&|types}",
-  "create_relationship": "http://localhost:7474/db/data/node/6/relationships",
-  "data": {
-    "desc": "Roll a party"
-  }*/
 
 }
 
 object TaskManager {
 
-  //import Cypher._
+  case class Task(desc: String)
 
-  //type CypherResponseData = List[String]
-
-  //case class CypherResponse(columns: List[String], data: CypherResponseData)
-  //List[List[String]]
-  //case class CypherResponseData()
+  case class TaskTree(task: Task, subTasks: List[TaskTree] = List())
 
   val dbUri = "http://localhost:7474/db/data/cypher"
 
@@ -97,7 +85,19 @@ object TaskManager {
 
   def getTasks = Http(request) map { response =>
     val bodyAsString = response.getResponseBody("utf-8")
-    Json.parse(bodyAsString).as[Cypher.Response[Cypher.Task]]
+    val asListOfNodes = Json.parse(bodyAsString).as(Cypher.Response.reader(list(Cypher.Node.reader(Json.reads[Task]))))
+    val asTasks: List[List[Task]] = asListOfNodes.data map { row =>
+      (row map { column =>
+        column map { node =>
+          node.data
+        }
+      }).head
+    }
+
+    asTasks map { taskList =>
+      taskList.dropRight(1).foldRight(TaskTree(taskList.last)){ (prev, acc) => TaskTree(prev, List(acc)) }
+    }
+
   }
 
 }
